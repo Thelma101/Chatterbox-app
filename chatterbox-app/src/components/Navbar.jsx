2a3487f17aaaaf547d5fae1d38ebf8b28fe97ac3
DCTP-BE <akpata.thelma@gmail.com>
2024-06-24 20:12:05 +0100

    v1

--- chatterbox-app/src/components/Navbar.jsx
+++ chatterbox-app/src/components/Navbar.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react';
-import SigninImg from '../img/googlesingin'
+import SigninImg from '../img/googlesingin'; // Ensure this path is correct
+import './Navbar.css'; // Ensure you have a CSS file for styling
 
 const Navbar = () => {
-  const [user, setUser] = useState(false)
+  const [user, setUser] = useState(false);
 
   const googleSignIn = () => {
     if (user) {
       return (
         <button className="sign-in" onClick={() => setUser(false)}>Sign Out</button>
-      )
+      );
     }
     return (
       <button className="sign-in" onClick={() => setUser(true)}>Sign In</button>
-    )
+    );
   }
-
 
   return (
     <div className="navbar">
@@ -22,7 +22,7 @@ const Navbar = () => {
         <img src={SigninImg} alt="logo" />
       </div>
       <div className="google-signin">
-        <googleSignin />
+        {googleSignIn()}
       </div>
     </div>
   );
