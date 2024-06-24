50baff8d6adf735b36dbde978d1d0b410f9ab137
DCTP-BE <akpata.thelma@gmail.com>
2024-06-24 19:33:10 +0100

    v1

--- chatterbox-app/src/components/Navbar.jsx
+++ chatterbox-app/src/components/Navbar.jsx
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import SigninImg from '../img/googlesingin'
 
 const Navbar = () => {
+  const [user, setUser] = useState(false)
+
+  const googleSignIn = () => {
+    if (user) {
+      return (
+        <button className="sign-in" onClick={() => setUser(false)}>Sign Out</button>
+      )
+    }
+    return (
+      <button className="sign-in" onClick={() => setUser(true)}>Sign In</button>
+    )
+  }
+
+
   return (
     <div className="navbar">
       <div className="logo">
         <img src={SigninImg} alt="logo" />
       </div>
       <div className="google-signin">
-        <GoogleSignin />
+        <googleSignin />
       </div>
     </div>
   );
