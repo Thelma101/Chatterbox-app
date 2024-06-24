0cd86db776a8e8ba430a029dcef8d625537e5fa5
DCTP-BE <akpata.thelma@gmail.com>
2024-06-24 14:20:21 +0100

    chatterbox app firs push

---
+++ chatterbox-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ChatBox from './components/ChatBox';
+import Message from './components/Message';
+import Navbar from './components/Message';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
