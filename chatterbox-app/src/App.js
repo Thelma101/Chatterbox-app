2578b2a4814f9a3299759cc0b40fd50e9c02b0f5
DCTP-BE <akpata.thelma@gmail.com>
2024-06-24 14:30:59 +0100

    v1

--- chatterbox-app/src/App.js
+++ chatterbox-app/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import ChatBox from './components/ChatBox';
 import Message from './components/Message';
-import Navbar from './components/Message';
-
+import Navbar from './components/Navbar';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      {!user ? <Welcome /> : <ChatBox />} 
     </div>
   );
 }
