4a677afaa79cf83491b25d8856c210a58b75302d
DCTP-BE <akpata.thelma@gmail.com>
2024-06-27 21:37:38 +0100

    v1

--- chatterbox-app/src/components/SendMessage.jsx
+++ chatterbox-app/src/components/SendMessage.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { auth } from "../firebase"
+import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
 
-const SendMessage = () =>{
-
+const SendMessage = () => {
     return (
-        <div>
+      <form className="send-message">
+        <label htmlFor="messageInput" hidden>
+          Enter Message
+        </label>
+        <input
+          id="messageInput"
+          name="messageInput"
+          type="text"
+          className="form-input__input"
+          placeholder="type message..."
+        />
+        <button type="submit">Send</button>
+      </form>
+    );
+  };
 
-        </div>
-    )
-}
-
+export default SendMessage;
