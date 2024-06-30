af14e5408b2deb50773e781e57dd2cf316c9d0e4
DCTP-BE <akpata.thelma@gmail.com>
2024-06-30 21:04:41 +0100

    v1

--- chatterbox-app/src/components/SendMessage.jsx
+++ chatterbox-app/src/components/SendMessage.jsx
@@ -22,7 +22,7 @@ const SendMessage = () => {
                 createdAt: serverTimestamp(),
                 uid,
               });
-              setMessage("");
+              setMessage('');
           }
         
     
